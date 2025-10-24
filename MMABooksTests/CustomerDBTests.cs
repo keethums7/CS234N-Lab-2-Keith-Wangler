d08c31738855b56bcbcae2fa398dd407ce9e3f6d
Keith Wangler <wanglerk@my.lanecc.edu>
2025-10-23 22:06:09 -0700

    Finishing Lab 2 work for now. All tests for customer, customerDB, product, and productDB classes are in place.

--- MMABooksTests/CustomerDBTests.cs
+++ MMABooksTests/CustomerDBTests.cs
@@ -13,30 +13,36 @@ namespace MMABooksTests
     public class CustomerDBTests
     {
         private Customer c;
+        private Customer customer;
 
         [SetUp]
         public void Setup()
         {
             c = new Customer();
-        }
-
-        [Test]
-        public void TestGetCustomer()
-        {
-            //c = new Customer();
-            c = CustomerDB.GetCustomer(1);
-            Assert.AreEqual(1, c.CustomerID);
-        }
-
-        [Test]
-        public void TestCreateCustomer()
-        {
             c.Name = "Mickey Mouse";
             c.Address = "101 Main Street";
             c.City = "Orlando";
             c.State = "FL";
             c.ZipCode = "10101";
 
+            Customer customer = new Customer();
+        }
+
+        [Test]
+        public void TestGetCustomer()
+        {
+            // test getting the first customer
+            // by first filling in a new empty
+            // customer and comparing
+            customer = CustomerDB.GetCustomer(1);
+            Assert.AreEqual(1, customer.CustomerID);
+        }
+
+        [Test]
+        public void TestCreateCustomer()
+        {
+            // setup gives us a default mickey mouse
+            // customer to work with for testing
             int customerID = CustomerDB.AddCustomer(c);
             c = CustomerDB.GetCustomer(customerID);
             Assert.AreEqual("Mickey Mouse", c.Name);
@@ -45,23 +51,39 @@ namespace MMABooksTests
         [Test]
         public void TestDeleteCustomer() 
         {
-            Customer customer = new Customer();
-            customer = CustomerDB.GetCustomer(1);
-            Console.WriteLine(customer);
-
             // Test that it won't delete
             // an invalid (empty) customer object
-            Assert.False(CustomerDB.DeleteCustomer(c));
+            Assert.Throws<NullReferenceException>(() => CustomerDB.DeleteCustomer(customer));
+
+            // test that it must be an exact match, messing up fields
+            // on an otherwise correct customer
+            customer = CustomerDB.GetCustomer(1);
+            customer.Name = "Testing";
+            customer.Address = "123";
+            Assert.False(CustomerDB.DeleteCustomer(customer));
 
             // Test that a known existing
-            // customer can be deleted
+            // customer can be deleted (1)
+            customer = CustomerDB.GetCustomer(1);
             Assert.True(CustomerDB.DeleteCustomer(customer));
-
-            // Readd and update the customer to reset the change
-            CustomerDB.AddCustomer(customer);
         }
 
         [Test]
-        public void TestUpdateCustomer() { }
+        public void TestUpdateCustomer() {
+
+            // Test with an empty (invalid) old customer
+            // Expecting it not to update anything
+            Assert.Throws<NullReferenceException>(() => CustomerDB.UpdateCustomer(customer, c));
+
+            // We expect null here too, since this new customer
+            // isn't a 100% match
+            customer = CustomerDB.GetCustomer(2);
+            Assert.False(CustomerDB.UpdateCustomer(c, customer));
+
+            // use a test customer (mickey) to
+            // update an existing customer (2)
+            customer = CustomerDB.GetCustomer(2);
+            Assert.True(CustomerDB.UpdateCustomer(customer, c));
+        }
     }
 }
